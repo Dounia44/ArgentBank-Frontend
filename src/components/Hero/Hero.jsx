c676ec9e50226fe720bc907211a22c2bc92ff68c
Dounia <dounia.huber@gmail.com>
2025-12-05 10:31:54 +0100

    feat: intégration du composant Hero avec contenu dynamique dans Home - Création du composant Hero.jsx (props dynamiques) - Stylisation du composant : SCSS BEM (Hero.scss) - Support des images responsives via src et srcSet - Inclusion du composant Hero dans Home.jsx

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import "./Hero.scss";
+
+function Hero({ src, srcSet, width, height, alt, title, subtitles, text }) {
+  return (
+    <div className="hero">
+      <img
+        className="hero__img"
+        src={src}
+        srcSet={srcSet}
+        width={width}
+        height={height}
+        alt={alt}
+      ></img>
+      <section className="hero__content">
+        <h2 className="sr-only">{title}</h2>
+        {subtitles.map((sub, index) => (
+          <p key={index} className="hero__subtitle">
+            {sub}
+          </p>
+        ))}
+        <p className="hero__text">{text}</p>
+      </section>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import Hero from "../components/Hero/Hero";
+import img400 from "../assets/images/bank-tree-400.webp";
+import img800 from "../assets/images/bank-tree-800.webp";
+import img1200 from "../assets/images/bank-tree-1200.webp";
+import img1600 from "../assets/images/bank-tree-1600.webp";
+
 function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <p>coucou</p>
+      <Hero
+        src={img400}
+        srcSet={`${img400} 400w, ${img800} 800w, ${img1200} 1200w, ${img1600} 1400w`}
+        width={400}
+        height={267}
+        alt="Bank tree"
+        title="Promoted Content"
+        subtitles={["No fees.", "No minimum deposit.", "High interest rates."]}
+        text="Open a savings account with Argent Bank today!"
+      />
     </>
   );
 }

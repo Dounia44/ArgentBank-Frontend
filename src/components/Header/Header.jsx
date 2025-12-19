ed5f452692080d8be06f73758a96cd8d12bd4414
Dounia <dounia.huber@gmail.com>
2025-12-19 10:25:49 +0100

    feat: affichage conditionnel du header et déconnexion

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.scss";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleUser,
@@ -8,7 +8,21 @@ import {
 import Logo from "../../assets/images/argentBankLogo.png";
 import Logo2x from "../../assets/images/argentBankLogo@2x.png";
 
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/auth.slice";
+
 function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
+  const userName = useSelector((state) => state.user.currentUser?.userName);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login", { replace: true });
+  };
+
   return (
     <nav className="main-nav">
       <Link className="main-nav__logo" to="/">
@@ -22,19 +36,31 @@ function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
+
       <div>
-        <Link className="main-nav__item" to="/Login">
-          <FontAwesomeIcon icon={faCircleUser} />
-          Sign In
-        </Link>
-        <Link className="main-nav__item" to="/Profile">
-          <FontAwesomeIcon icon={faCircleUser} />
-          Tony
-        </Link>
-        <Link className="main-nav__item" to="/Login">
-          <FontAwesomeIcon icon={faRightFromBracket} />
-          Sign Out
-        </Link>
+        {!token && (
+          <Link className="main-nav__item" to="/Login">
+            <FontAwesomeIcon icon={faCircleUser} />
+            Sign In
+          </Link>
+        )}
+
+        {token && (
+          <>
+            <Link className="main-nav__item" to="/Profile">
+              <FontAwesomeIcon icon={faCircleUser} />
+              {userName}
+            </Link>
+            <Link
+              className="main-nav__item"
+              to={undefined}
+              onClick={handleLogout}
+            >
+              <FontAwesomeIcon icon={faRightFromBracket} />
+              Sign Out
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
